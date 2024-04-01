c0e9277776f67658a7e92b726d4433af1c22b0ce
ali tarichi <tarichiali@gmail.com>
2024-04-01 18:53:28 +0300

    still not showing the pages after auth

--- context/authContext.js
+++ context/authContext.js
@@ -1,53 +1,46 @@
-import {createContext, useContext, useEffect, useState} from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
-export const AuthContextProvider = ({children}) => {
-    const [user, setUser] = useState(null);
-    const [isAuthenticated, setIsAuthenticated] = useState(undefined);
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(undefined);
 
-useEffect(() => {
+  useEffect(() => {
     // onAuthStateChange
 
     setTimeout(() => {
-        setIsAuthenticated(true);
+      setIsAuthenticated(true);
     }, 3000);
-}, [])
+  }, []);
 
-const login = async (email, password) => {
+  const login = async (email, password) => {
     try {
+    } catch (e) {}
+  };
 
-    }catch(e) {
-
-    }
-}
-
-const logout = async () => {
+  const logout = async () => {
     try {
-
-    }catch(e) {
-        
-    }
-}
-const register = async (email, password, username, profileUrl) => {
+    } catch (e) {}
+  };
+  const register = async (email, password, username, profileUrl) => {
     try {
+    } catch (e) {}
 
-    }catch(e) {
-        
-    }
-
-return (
-    <AuthContext.Provider value={{user,isAuthenticated, login, register, logout}}>
+    return (
+      <AuthContext.Provider
+        value={{ user, isAuthenticated, login, register, logout }}
+      >
         {children}
-    </AuthContext.Provider>
-)
-}
-}
+      </AuthContext.Provider>
+    );
+  };
+};
 export const useAuth = () => {
-    const value = useContext(AuthContext);
+  const value = useContext(AuthContext);
 
-    if(!value) {
-        throw new Error("useAuth must be used within an AuthContextProvider");
-    }
-    return value;
-}
+  if (!value) {
+    throw new Error("useAuth must be used within an AuthContextProvider");
+  }
+  return value;
+};

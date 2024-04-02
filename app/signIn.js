4a25e23d287b3b0b9ddf1d2fffb8cfeee809b0fe
ali tarichi <tarichiali@gmail.com>
2024-04-02 12:03:40 +0300

    adding inputs and placeholders to login

--- app/signIn.js
+++ app/signIn.js
@@ -1,10 +1,11 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TextInput } from "react-native";
 import React from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import { StatusBar } from "expo-status-bar";
+import { Octicons } from "@expo/vector-icons";
 
 export default function SignIn() {
   return (
@@ -21,6 +22,42 @@ export default function SignIn() {
             resizeMode="contain"
             source={require("../assets/images/login.png")}
           />
+        </View>
+
+        <View>
+          <Text
+            style={{ fontSize: hp(4) }}
+            className="font-bold tracking-wider text-center text-neutral-800"
+          >
+            Sign In
+          </Text>
+          {/* inputs */}
+          <View className="gap-4">
+            <View
+              style={{ height: hp(7) }}
+              className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl "
+            >
+              <Octicons name="mail" size={hp(2.7)} color="gray" />
+              <TextInput
+                style={{ fontSize: hp(2) }}
+                className="flex-1 font-semibold text-neutral-700"
+                placeholder="Email Address"
+                placeholderTextColor="gray"
+              />
+            </View>
+            <View
+              style={{ height: hp(7) }}
+              className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl "
+            >
+              <Octicons name="lock" size={hp(2.7)} color="gray" />
+              <TextInput
+                style={{ fontSize: hp(2) }}
+                className="flex-1 font-semibold text-neutral-700"
+                placeholder="Password"
+                placeholderTextColor="gray"
+              />
+            </View>
+          </View>
         </View>
       </View>
     </View>

fcaa7b4bdfad0a5485da84c941555f1ea555bf55
ali tarichi <tarichiali@gmail.com>
2024-04-04 17:30:35 +0300

    classname is not working so i did a shortcut and declleared all the items as a style component

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,13 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 import React from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+const ios = Platform.OS === "ios";
 export default function HomeHeader() {
+  const { top } = useSafeAreaInsets();
   return (
-    <View>
+    <View
+      style={{
+        paddingTop: ios ? top : top + 10,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingHorizontal: wp("5%"),
+        backgroundColor: "#00C7FF", // bg-indigo-400
+        paddingBottom: 6, // pb-6
+        borderRadius: wp("5%"), // rounded-b-3xl
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 4,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 4.65,
+        elevation: 8,
+      }}
+    >
       <Text>HomeHeader</Text>
     </View>
   );

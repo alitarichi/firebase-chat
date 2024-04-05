1c59003589482a99efffe0c49da6f2e66ee741d5
ali tarichi <tarichiali@gmail.com>
2024-04-05 14:12:49 +0300

    custom menu component has created

---
+++ components/CustomMenuItems.js
@@ -0,0 +1,25 @@
+import { Text, View } from "react-native";
+import { MenuOption } from "react-native-popup-menu";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+
+export const MenuItem = ({ text, action, value, icon }) => {
+  return (
+    <MenuOption onSelect={() => action(value)}>
+      <View
+        sstyle={{
+          flexDirection: "row",
+          paddingHorizontal: 4,
+          paddingVertical: 1,
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Text>icon</Text>
+        <Text>text</Text>
+      </View>
+    </MenuOption>
+  );
+};

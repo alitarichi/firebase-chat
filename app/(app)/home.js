2c8a0fdb17bd8c1c2c7c33eb3864a457353bd39e
ali tarichi <tarichiali@gmail.com>
2024-04-05 17:26:32 +0300

    chatlist inital commit

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,18 +1,28 @@
-import { View, Text, Pressable } from "react-native";
-import React from "react";
+import { View, Text, Pressable, ActivityIndicator } from "react-native";
+import React, { useState } from "react";
 import { useAuth } from "../../context/authContext";
+import { StatusBar } from "expo-status-bar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default function Home() {
   const { logout, user } = useAuth();
+  const [users, setUsers] = useState([1, 2, 3]);
   const handleLogout = async () => {
     await logout();
   };
   return (
     <View className="flex-1 bg-white">
-      {/*<Button title="Sign Out" />*/}
-      <Pressable onPress={handleLogout}>
-        <Text>Sign Out</Text>
-      </Pressable>
+      <StatusBar style="light" />
+      {users.lenght > 0 ? (
+        <ChatList users={users} />
+      ) : (
+        <View className="flex items-center" style={{ top: hp(30) }}>
+          <ActivityIndicator size="larger" color="#00C7FF" />
+        </View>
+      )}
     </View>
   );
 }

780b5682e2a3f308ee9c2ce00597bc9759bf4aee
ali tarichi <tarichiali@gmail.com>
2024-04-08 11:33:32 +0300

    chatitem inital commit

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,26 +1,30 @@
 import { View, Text, Pressable, ActivityIndicator } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../../context/authContext";
 import { StatusBar } from "expo-status-bar";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import ChatList from "../../components/ChatList";
 
 export default function Home() {
   const { logout, user } = useAuth();
   const [users, setUsers] = useState([1, 2, 3]);
-  const handleLogout = async () => {
-    await logout();
+  useEffect(() => {
+    if (user?.uid) getUsers();
+  }, []);
+  const getUsers = async () => {
+    // fetch users
   };
   return (
     <View className="flex-1 bg-white">
       <StatusBar style="light" />
-      {users.lenght > 0 ? (
+      {users.length > 0 ? (
         <ChatList users={users} />
       ) : (
         <View className="flex items-center" style={{ top: hp(30) }}>
-          <ActivityIndicator size="larger" color="#00C7FF" />
+          <ActivityIndicator size="large" color="#00C7FF" />
         </View>
       )}
     </View>
